63f4ece525851ef091bf3bb6b773a78daa4dda23
Kingsley Etoka <etokakingsley@gmail.com>
2022-10-04 16:17:43 +0100

    observed state from UI

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { loginPageActions } from '../store/action/auth.action';
+import { authReducer } from '../store/reducer/auth.reducer';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  isLoading$: Observable<boolean>
 
-  constructor() { }
+  constructor(private readonly store : Store) {
+      this.isLoading$ = store.select(authReducer.selectIsLoginLoading)
+   }
 
   ngOnInit(): void {
+    setTimeout(
+      ()=> this.store.dispatch(loginPageActions.signIn({email: "etoka@gmail.com", password: "12345"}))
+      ,9000
+    )
   }
 
 }
