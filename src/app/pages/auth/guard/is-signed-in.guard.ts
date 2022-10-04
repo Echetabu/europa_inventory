afc353fef4d85b6fed5443da258cbefd45536d87
Kingsley Etoka <etokakingsley@gmail.com>
2022-10-04 15:46:33 +0100

    implemented the canActivateChildGuard

--- src/app/pages/auth/guard/is-signed-in.guard.ts
+++ src/app/pages/auth/guard/is-signed-in.guard.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class IsSignedInGuard implements CanActivate {
+export class IsSignedInGuard implements CanActivate, CanActivateChild {
   constructor(private readonly router: Router) {}
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
