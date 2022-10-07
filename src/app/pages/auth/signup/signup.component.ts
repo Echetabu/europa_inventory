bd88b7d9285970632d1ecf19cc44b0c536a62e57
Echetabu <echetabuebuka@gmail.com>
2022-10-07 14:40:46 +0100

    Added signup page

--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { creatAuthReducer } from '../store/reducer/create-auth.reducer';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  isLoading$!: Observable<boolean>;
+  constructor(private readonly store : Store) { this.isLoading$ = store.select(creatAuthReducer.selectIsSignupLoading) }
 
   ngOnInit(): void {
   }
